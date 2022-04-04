7627a4ba5eb462e9589dcfcd9e368fca976a9c6e
Journeyeur <ericillick@gmail.com>
2022-04-04 17:02:53 -0600

    asdf

--- openstackfunctions.py
+++ openstackfunctions.py
@@ -7,6 +7,9 @@ openstack.enable_logging(debug=True)
 # Initialize connection
 conn = openstack.connect(cloud='admin')
 
+conn.create_server(
+    'test1',image= 'a2b43ae8-7ce7-4512-a4f9-994c0fa06ead',flavor='m1.tiny',wait=True,auto_ip=True)
+
 # List the servers
-for server in conn.compute.servers():
-    print(server.to_dict())
+# for server in conn.compute.servers():
+#     print(server.to_dict())
