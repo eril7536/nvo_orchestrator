2b24d55b713ca8e4646c455c5f81a14147792ff3
Battle School <battleschool@Battles-MacBook-Pro-2.local>
2022-04-04 13:37:04 -0600

    all the stuffs

--- orchestrator.py
+++ orchestrator.py
@@ -10,13 +10,10 @@ def openstack():
     deployOsNw()
     deployOsSec()
 
-def bgp(local_as_ryu, router_id_ryu, neighbor_id_ryu, remote_as_ryu, local_as_q, router_id_q, neighbor_id_q, remote_as_q):
-    ryuConf(local_as_ryu, router_id_ryu, neighbor_id_ryu, remote_as_ryu)
-    quaggaConf(local_as_q, router_id_q, neighbor_id_q, remote_as_q)
 
 def docker():
+    dockerfunctions()
     
 if __name__ == "__main__":
     # openstack()
     docker()
-    # bgp(local_as_ryu, router_id_ryu, neighbor_id_ryu, remote_as_ryu, local_as_q, router_id_q, neighbor_id_q, remote_as_q)
