9877c42a49b818e51477889194fc37c451fbd66c
Journeyeur <ericillick@gmail.com>
2022-04-04 23:32:39 -0600

    asdf

--- orchestrator.py
+++ orchestrator.py
@@ -1,9 +1,16 @@
 from openstack_builder import build_vns, build_vms, build_sec_groups, build_router
+from dockerfunctions import docker_file_generate, spin_up_dockers
 
 def openstack():
     # build_vns()
     # build_vms()
     # build_sec_groups()
     build_router()
+# def docker():
+#     if docker_file_generate == True:
+#         print("spin up dockers")
+#         spin_up_dockers()
+
 if __name__ == "__main__":
     openstack()
+    # docker()
